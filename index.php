9f64d777172d594598784c82e5639604aa408ef0
Emm19-aduSarks <78122918+Emm19-aduSarks@users.noreply.github.com>
2024-02-10 17:02:23 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo('Nkunim is here');
                 error_reporting(E_ALL);
                 ini_set('display_errors', 1);
 
@@ -42,8 +41,8 @@ echo('Nkunim is here');
     <link href='https://fonts.googleapis.com/css?family=Poppins:400,400i,500,600,700' rel='stylesheet' type='text/css'>
 
     <!-- Stylesheets ============================================= -->
-    <link href="/CSS/styleOne.css" rel="stylesheet">
-    <link href="/CSS/styleTwo.css" rel="stylesheet">
+    <link href="/CSS/styleOne.css" rel="stylesheet" type="text/css">
+    <link href="/CSS/styleTwo.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="preconnect" href="https://fonts.googleapis.com">
